2ec67c047412efceed749693ab88f17a7b17cb34
Luiz Rocha <luiz.felipe@dnx.solutions>
2020-06-19 15:07:20 +1000

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+# AWS Backup vault
+resource "aws_backup_vault" "backup_vault" {
+  # count       = var.enabled && var.vault_name != null ? 1 : 0
+  name        = var.vault_name
+  # kms_key_arn = var.vault_kms_key_arn
+  # tags        = var.tags
+}
+
+# AWS Backup plan
+resource "aws_backup_plan" "backup_plan" {
+  # count = var.enabled ? 1 : 0
+  name  = var.plan_name
+
+  rule {
+    rule_name         = var.rule_name
+    target_vault_name = aws_backup_vault.example.name
+    schedule          = var.rule_schedule
+    start_window      = var.rule_start_window
+    completion_window = var.rule_completion_window
+    lifecycle {
+      cold_storage_after  = var.rule_lifecycle_cold_storage_after
+      delete_after        = var.rule_lifecycle_delete_after
+    }
+  }
+}
+
