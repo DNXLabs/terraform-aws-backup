e29e46027aedd21209fc446167897cd00737bcb6
Luiz Rocha <luiz.felipe@dnx.solutions>
2020-06-27 10:05:38 +1000

    Fixing code format

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # AWS Backup vault
 resource "aws_backup_vault" "backup_vault" {
   # count       = var.enabled && var.vault_name != null ? 1 : 0
-  name        = var.vault_name
+  vault_name = var.vault_name
   # kms_key_arn = var.vault_kms_key_arn
   # tags        = var.tags
 }
@@ -9,7 +9,7 @@ resource "aws_backup_vault" "backup_vault" {
 # AWS Backup plan
 resource "aws_backup_plan" "backup_plan" {
   # count = var.enabled ? 1 : 0
-  name  = var.plan_name
+  name = var.plan_name
 
   rule {
     rule_name         = var.rule_name
@@ -18,8 +18,8 @@ resource "aws_backup_plan" "backup_plan" {
     start_window      = var.rule_start_window
     completion_window = var.rule_completion_window
     lifecycle {
-      cold_storage_after  = var.rule_lifecycle_cold_storage_after
-      delete_after        = var.rule_lifecycle_delete_after
+      cold_storage_after = var.rule_lifecycle_cold_storage_after
+      delete_after       = var.rule_lifecycle_delete_after
     }
   }
 }
