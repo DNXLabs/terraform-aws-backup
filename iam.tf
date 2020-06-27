ab23ee35a58fa5d60b49eb6bf57b1e39c19f7b08
lzrocha <luizfel@gmail.com>
2020-06-27 10:23:49 +1000

    Adding iam.tf

---
+++ iam.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_role" "backup_role" {
+  name               = "aws-backup-role"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": ["sts:AssumeRole"],
+      "Effect": "allow",
+      "Principal": {
+        "Service": ["backup.amazonaws.com"]
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "backup_policy_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
+  role       = aws_iam_role.backup_role.name
+}
--- selection.tf
+++ selection.tf
@@ -1,26 +1,3 @@
-resource "aws_iam_role" "backup_role" {
-  name               = "aws-backup-role"
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": ["sts:AssumeRole"],
-      "Effect": "allow",
-      "Principal": {
-        "Service": ["backup.amazonaws.com"]
-      }
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "backup_policy_attach" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
-  role       = aws_iam_role.backup_role.name
-}
-
 resource "aws_backup_selection" "backup_selection" {
   # count = var.enabled ? length(local.selections) : 0
 
