5785f454c5507a7ca9798a7f26ad0b3e5d0257cd
Felipe Ferreira <felipesqf@gmail.com>
2023-05-05 14:27:29 +1000

    chane backup example

--- examples/simple-plan-using-tags.tf
+++ examples/simple-plan-using-tags.tf
@@ -1,44 +1,23 @@
 
 module "backups" {
   source = "git::https://github.com/DNXLabs/terraform-aws-backup?ref=1.0.2"
-
-  name          = "production-by-tags"
-  rule_schedule = "cron(0 12 * * ? *)" # 12:00pm UTC -> 10:00pm AEST (http://crontab.org/)
-
-  # Selection of resources by tag
-  # Supported resources Aurora, DynamoDB, EBS, EC2, FSx, EFS, RDS, Storage Gateway 
-  selection_tag_key   = "Environment"
-  selection_tag_value = "production"
-
-  enabled = true
-  vault_kms_key_arn = ""
-  # selection_resources = ["arn:aws:s3:::s3-test"]
-  rule = [{
-    rule_name         = "backup-rule"
-    target_vault_name = "backup-vault"
-    schedule          = "cron(15 * ? * * *)"
-    start_window      = 60
-    completion_window = 120
-    enable_continuous_backup = true
-    lifecycle_cold_storage_after = 30
-    lifecycle_delete_after = 35
+  # source = "./modules/backup"
+  enabled = local.workspace.backups.enabled
+  selection_tag_key   = local.workspace.backups.selection_tag_key
+  selection_tag_value = local.workspace.backups.selection_tag_value
+  for_each = { for rules in local.workspace.backups.rules : rules.rule_name => rules }
+  rule = {
+    rule_name         = local.workspace.backups.rule_name
+    target_vault_name = local.workspace.backups.target_vault_name
+    schedule          = local.workspace.backups.schedule
+    start_window      = local.workspace.backups.start_window
+    completion_window = local.workspace.backups.completion_window
+    enable_continuous_backup = local.workspace.backups.enable_continuous_backup
+    lifecycle_cold_storage_after = local.workspace.backups.lifecycle_cold_storage_after
+    lifecycle_delete_after = local.workspace.backups.lifecycle_delete_after
     lifecycle = {
-      cold_storage_after = 30
-      delete_after       = 35
+      cold_storage_after = local.workspace.backups.lifecycle_cold_storage_after
+      delete_after       = local.workspace.backups.lifecycle_delete_after
     }
-    },
-    {
-    rule_name         = "backup-test"
-    target_vault_name = "backup-vault-test"
-    schedule          = "cron(15 * ? * * *)"
-    start_window      = 60
-    completion_window = 120
-    enable_continuous_backup = false
-    lifecycle_cold_storage_after = 30
-    lifecycle_delete_after = 120
-    lifecycle = {
-      cold_storage_after = 30
-      delete_after       = 120
-    }
-    }]
+  }
 }
